d4c372d175efdf3f3b59c8c4cbc293b6c2898236
Yong Guan <gyong9@gmail.com>
2013-12-02 20:03:12 -0800

    use same array elements for all tranformation

--- app/js/data/stock-return-dw.js
+++ app/js/data/stock-return-dw.js
@@ -8,26 +8,33 @@ define(['exports', 'data/sp500-annual-return'], function (exports, sp500AnnualRe
         stockReturn = {};
     MicroEvent.mixin(stockReturn);
 
+    function transformData(data) {
+        _.each(data, function (value) {
+            value['S&P 500 Annual Return'] = value.AnnualReturn / 100;
+        });
+    }
+
     function calculateInvestmentReturn(initialInvestment, percentChanged) {
         return (100 + percentChanged) / 100 * initialInvestment;
     }
 
     function filterStockReturn(initialInvestment, startYear) {
-        var newData = from(sp500Data)
-            .where('$Year >= @', startYear)
-            .toArray();
+        var startIndex = _.findIndex(sp500Data, function (value) {
+                return value.Year >= startYear;
+            }),
+            newData = sp500Data.slice(startIndex, sp500Data.length);
 
         _.each(newData, function (value, index) {
             var preIndex = index - 1,
                 investment = preIndex < 0 ? initialInvestment : calculateInvestmentReturn(newData[preIndex].Investment, newData[preIndex].AnnualReturn);
             value.Investment = investment;
-            value['S&P 500 Annual Return'] = value.AnnualReturn / 100;
         });
 
         stockReturn.data = newData;
         stockReturn.trigger('change');
     }
 
+    transformData(sp500Data);
     stockReturn.filterStockReturn = filterStockReturn;
 
     exports.stockReturn = stockReturn;
